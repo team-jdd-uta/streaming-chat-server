3a54786453e0f36dcff24f11ad2f6f536d3f5b4f
정찬혁 <stonedool@naver.com>
2026-02-17 00:32:28 +0900

    DTO 구조 변경

--- src/main/java/com/example/demo/model/DTO/WatchHistoryDTO.java
+++ src/main/java/com/example/demo/model/DTO/WatchHistoryDTO.java
@@ -1,14 +1,22 @@
 package com.example.demo.model.DTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import java.time.LocalDateTime;
 
 @Data
 @Getter
 @Setter
 public class WatchHistoryDTO {
-    public int videoId;
+    @JsonProperty("streamerId")
+    public Long videoId;
+    @JsonProperty("user_id")
+    public String userId;
     public String videoName;
-    public String startedAt;
+    @JsonProperty("started_at")
+    public LocalDateTime startedAt;
+    @JsonProperty("ended_at")
+    public LocalDateTime endedAt;
 }
